1bc7b453761b47d0da0857f60ca8c4fdcf34498c
james_stocktonj <james.stocktonj@gmail.com>
2021-07-18 16:28:47 +0100

    Working path reduction

--- pathredux.py
+++ pathredux.py
@@ -10,7 +10,7 @@ from pathfinding import *
 
 def deleteToNode(maze_grid, x, y):
 
-    #array of the different squares to delet
+    #array of the different squares to delete
     listToDelete = [[x, y]]
 
 
@@ -21,48 +21,50 @@ def deleteToNode(maze_grid, x, y):
 
         #get the previous coord
         prevSquare = listToDelete[len(listToDelete) - 1]
+        nextSquares = findNextPaths(maze_grid, prevSquare[0], prevSquare[1])
 
-        #if greater than two then must be node
-        if numNewPaths(maze_grid, prevSquare[0], prevSquare[1]) > 2:
 
-            #return delete list
+        #if not the first node (dead end) then delete previous square to stop repeating
+        if len(listToDelete) > 1:
+
+            if listToDelete[len(listToDelete) - 2] in nextSquares:
+
+                nextSquares.remove(listToDelete[len(listToDelete) - 2])
+
+
+        #if node is not caught then this error will show up
+        if len(nextSquares) > 1:
+            print("REDUCTION ERROR\nThere are more than one squares to reduce")
+        
+
+        #if the next square is a node then return
+        if numNewPaths(maze_grid, nextSquares[0][0], nextSquares[0][1]) > 2:
             return listToDelete
-
+        
+        #add the next square to the list to delete
         else:
-
-            #get next paths 
-            nextSquare = findNextPaths(maze_grid, prevSquare[0], prevSquare[1])
+            listToDelete.append(nextSquares[0])
 
 
-            #if not the first node in the array then delete the previous square
-            if len(listToDelete) != 1:
-
-                nextSquare.remove(listToDelete[len(listToDelete) - 2])
+        
 
 
-            #if the node isnt caught then this error message will display
-            if len(nextSquare) > 1:
 
-                print("REDUCTION ERROR\nThere are more than one squares to reduce")
-
-            #add the next square to list to delet
-            listToDelete.append(nextSquare[0])
+def path_reduction(maze_grid):
 
 
-def path_reduction(maze_grid, node_grid, maze_size):
+    for y in range(1, len(maze_grid) - 1):
+
+        for x in range(1, len(maze_grid[0]) - 1):
 
 
-    for y in range(1, maze_size[1] - 1):
-
-        for x in range(1, maze_size[0] - 1):
-
-            #if dead end then start reduction
-            if node_grid[y][x] == "D":
-                delSquares = deleteToNode(node_grid, x, y)
-
-                #print(delSquares)
+            #if dead end (and coord x, y is not a wall) then start reduction
+            if (numNewPaths(maze_grid, x, y) == 1) and (maze_grid[y][x] == 1):
+                
+                delSquares = deleteToNode(maze_grid, x, y)
 
                 if len(delSquares) != 0:
+
                     for sq in delSquares:
 
                         #set square to delete to 0 (black)
